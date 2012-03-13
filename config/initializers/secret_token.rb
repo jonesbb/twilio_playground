4bbb410b08526b497d6b3189ecda590e8db3fd8e
jonesbb <jonesbb@gmail.com>
2012-03-13 11:22:15 -0700

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+TwilioPlayground::Application.config.secret_token = '4ed443f9d113a6db2320270390287bf5f678ff889e16ed6380c2ff60a98d2d478d25fcf60f1ed3fba544a7aa2fdfde6a70bb3a3cf3117811c49417078dcde40c'
